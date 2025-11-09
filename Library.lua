2f27ce2a31e25ab69299edf9a3784c0c1b10daf5
宇星辰sy <160198513+SyndromeXph@users.noreply.github.com>
2025-11-09 11:56:58 +0800

    Change color scheme from HSV to RGB values

--- Library.lua
+++ Library.lua
@@ -79,16 +79,16 @@ local Library = {
 
     IsLightTheme = false,
     Scheme = {
-        BackgroundColor = Color3.fromHSV((tick() * 0.3) % 1, 0.8, 0.15),
-MainColor = Color3.fromHSV((tick() * 0.3) % 1, 0.8, 0.25),
-AccentColor = Color3.fromHSV((tick() * 0.5) % 1, 0.8, 1),
-OutlineColor = Color3.fromHSV((tick() * 0.3) % 1, 0.8, 0.4),
-FontColor = Color3.fromHSV((tick() * 0.5) % 1, 0.8, 1),
-Font = Font.fromEnum(Enum.Font.Code),
+        BackgroundColor = Color3.fromRGB(15, 15, 15),
+        MainColor = Color3.fromRGB(25, 25, 25),
+        AccentColor = Color3.fromRGB(125, 85, 255),
+        OutlineColor = Color3.fromRGB(40, 40, 40),
+        FontColor = Color3.new(1, 1, 1),
+        Font = Font.fromEnum(Enum.Font.Code),
 
-Red = Color3.fromHSV((tick() * 0.5) % 1, 0.8, 1),
-Dark = Color3.fromHSV((tick() * 0.3) % 1, 0.8, 0.1),
-White = Color3.fromHSV((tick() * 0.5) % 1, 0.8, 1),
+        Red = Color3.fromRGB(255, 50, 50),
+        Dark = Color3.new(0, 0, 0),
+        White = Color3.new(1, 1, 1),
     },
 
     Registry = {},
